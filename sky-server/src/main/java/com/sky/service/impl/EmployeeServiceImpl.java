00d8451294d2530f666cdea6d540f421cc197f8d
wuyuchen <1305393331@qq.com>
2023-12-16 22:16:46 +0800

    第二次提交（day01）

--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -37,9 +37,9 @@ public class EmployeeServiceImpl implements EmployeeService {
             //账号不存在
             throw new AccountNotFoundException(MessageConstant.ACCOUNT_NOT_FOUND);
         }
-
+        password = DigestUtils.md5DigestAsHex(password.getBytes());
         //密码比对
-        // TODO 后期需要进行md5加密，然后再进行比对
+        //对前端传来的明文密码进行md5处理
         if (!password.equals(employee.getPassword())) {
             //密码错误
             throw new PasswordErrorException(MessageConstant.PASSWORD_ERROR);
