46584ef5031e2f16f93011bc5bf6fdf3aabd0e10
wuyuchen <1305393331@qq.com>
2023-12-18 19:33:10 +0800

    员工分页查询及启动禁止功能代码提交

--- sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -3,8 +3,10 @@ package com.sky.controller.admin;
 import com.sky.constant.JwtClaimsConstant;
 import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
 import com.sky.properties.JwtProperties;
+import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.EmployeeService;
 import com.sky.utils.JwtUtil;
@@ -13,13 +15,12 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
+
+import static com.github.pagehelper.util.ExecutorUtil.pageQuery;
 
 /**
  * 员工管理
@@ -83,5 +84,26 @@ public class EmployeeController {
         employeeService.save(employeeDTO);
         return Result.success();
     }
+    @GetMapping("/page")
+    @ApiOperation("员工分页查询")
+    public Result<PageResult> page(EmployeePageQueryDTO employeePageQueryDTO){
+        log.info("员工分页查询，参数为：{}",employeePageQueryDTO);
+        PageResult pageResult= employeeService.pageQuery(employeePageQueryDTO);
+        return Result.success(pageResult);
 
+    }
+
+    /**
+     * 启用禁用员工账号
+     * @param status
+     * @param id
+     * @return
+     */
+    @ApiOperation("启用禁用员工账号")
+    @PostMapping("status/{status}")
+    public Result startorstop(@PathVariable Integer status,Long id){
+        log.info("启用禁用员工账号：{}，{}",status,id);
+        employeeService.startorstop(status,id);
+        return Result.success();
+    }
 }
--- sky-server/src/main/java/com/sky/service/EmployeeService.java
+++ sky-server/src/main/java/com/sky/service/EmployeeService.java
@@ -2,7 +2,9 @@ package com.sky.service;
 
 import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
+import com.sky.result.PageResult;
 
 public interface EmployeeService {
 
@@ -14,4 +16,13 @@ public interface EmployeeService {
     Employee login(EmployeeLoginDTO employeeLoginDTO);
 /*新增员工实现方法*/
     void save(EmployeeDTO employeeDTO);
+/*分页查询方法*/
+    PageResult pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
+
+    /**
+     * 启用禁用员工账号
+     * @param status
+     * @param id
+     */
+    void startorstop(Integer status, Long id);
 }
