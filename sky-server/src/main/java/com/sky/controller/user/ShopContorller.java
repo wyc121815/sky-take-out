264936ee13628f7586d5cd9e4f81190c896c46bb
wuyuchen <1305393331@qq.com>
2024-01-14 14:12:33 +0800

    店铺营业状态设置代码提交

--- sky-server/src/main/java/com/sky/controller/user/ShopContorller.java
+++ sky-server/src/main/java/com/sky/controller/user/ShopContorller.java
@@ -1,4 +1,4 @@
-package com.sky.controller.admin;
+package com.sky.controller.user;
 
 import com.sky.result.Result;
 import io.swagger.annotations.Api;
@@ -8,21 +8,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.*;
 
-@RestController("adminShopController")
-@RequestMapping("/admin/shop")
+@RestController("userShopController")
+@RequestMapping("/user/shop")
 @Api(tags = "店铺相关接口")
 @Slf4j
 public class ShopContorller {
     public static final String KEY="SHOP_STATUS";
     @Autowired
     private RedisTemplate redisTemplate;
-    @PutMapping("/{status}")
-    @ApiOperation("设置店铺的营业状态")
-    public Result setStatus(@PathVariable Integer status){
-        log.info("设置店铺的营业状态为：{}",status==1?"营业中":"打烊中");
-        redisTemplate.opsForValue().set(KEY,status);
-        return Result.success();
-    }
+
     @GetMapping("/status")
     @ApiOperation("获取店铺的营业状态")
     public Result<Integer> getStatus(){
