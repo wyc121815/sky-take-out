9b8bfdb4511166f8935f4abcd3bcb87f860bc111
wuyuchen <1305393331@qq.com>
2024-02-28 13:43:02 +0800

    添加购物车代码提交

--- sky-pojo/src/main/java/com/sky/dto/ShoppingCartDTO.java
+++ sky-pojo/src/main/java/com/sky/dto/ShoppingCartDTO.java
@@ -4,7 +4,7 @@ import lombok.Data;
 import java.io.Serializable;
 
 @Data
-public class ShoppingCartDTO implements Serializable {
+public class  ShoppingCartDTO implements Serializable {
 
     private Long dishId;
     private Long setmealId;
---
+++ sky-server/src/main/java/com/sky/mapper/ShoppingCartMapper.java
@@ -0,0 +1,36 @@
+package com.sky.mapper;
+
+import com.sky.entity.ShoppingCart;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Update;
+
+import java.util.List;
+
+@Mapper
+public interface ShoppingCartMapper {
+
+    
+
+    /**
+     * 动态条件查询2
+     * @param shoppingCart
+     * @return
+     */
+    List<ShoppingCart> list(ShoppingCart shoppingCart);
+
+    /**
+     * 根据id修改商品数量
+     * @param shoppingCart
+     */
+    @Update("update shopping_cart set number = #{number} where id=#{id}")
+    void updateNumberById(ShoppingCart shoppingCart);
+
+    /**
+     * 插入数据
+     * @param shoppingCart
+     */
+    @Insert("insert into shopping_cart (name, user_id, dish_id, setmeal_id, dish_flavor, number, amount, image, create_time) " +
+            " values (#{name},#{userId},#{dishId},#{setmealId},#{dishFlavor},#{number},#{amount},#{image},#{createTime})")
+    void insert(ShoppingCart shoppingCart);
+}
